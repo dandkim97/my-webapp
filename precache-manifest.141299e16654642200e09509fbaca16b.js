08fec44f63d9d238a5dfe1760060741b6f665ead
Dan Kim <dan.dkyu.kim@gmail.com>
2019-06-22 22:55:25 -0400

    Updates

---
+++ precache-manifest.141299e16654642200e09509fbaca16b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502a70abaafd2877c0ba4c09bc3559d8",
+    "url": "/my-webapp/index.html"
+  },
+  {
+    "revision": "4b39abab6b2e32ee1a01",
+    "url": "/my-webapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/my-webapp/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "4b39abab6b2e32ee1a01",
+    "url": "/my-webapp/static/js/main.79eb5ce9.chunk.js"
+  },
+  {
+    "revision": "73daf9c2f500f45e372b",
+    "url": "/my-webapp/static/js/runtime~main.a20fa8fc.js"
+  }
+]);
